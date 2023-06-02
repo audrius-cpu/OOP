67019b63d11e79a3a9981381c68153eb6b9eaba1
audrius-cpu <115582149+audrius-cpu@users.noreply.github.com>
2023-06-02 13:07:58 +0300

    Update header.h

--- RODYKLES1/header.h
+++ RODYKLES1/header.h
@@ -1,31 +1,52 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.3.32929.385
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "RODYKLES1", "RODYKLES1.vcxproj", "{23B0BAD0-61A1-4688-860C-FBA7053445BE}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Debug|x64.ActiveCfg = Debug|x64
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Debug|x64.Build.0 = Debug|x64
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Debug|x86.ActiveCfg = Debug|Win32
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Debug|x86.Build.0 = Debug|Win32
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Release|x64.ActiveCfg = Release|x64
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Release|x64.Build.0 = Release|x64
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Release|x86.ActiveCfg = Release|Win32
-		{23B0BAD0-61A1-4688-860C-FBA7053445BE}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {90A7CFE9-76C1-42F1-84EB-FB2288284C30}
-	EndGlobalSection
-EndGlobal
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+
+using namespace std;
+
+class studentas
+{
+    private:
+
+    string vardas, pavarde;
+    int egzaminas;
+    double vidurkis, mediana;
+
+    public:
+
+        vector<int> pazymiai;
+
+        inline string vardo_nuskaitymas() const { return vardas; };
+        inline string pavardes_nuskaitymas() const { return pavarde; };
+        inline int egzamino_nuskaitymas() const { return egzaminas; };
+        inline double vidurkio_nuskaitymas() const { return vidurkis; };
+        inline double medianos_nuskaitymas() const { return mediana; };
+        const vector<int> pazymiu_nuskaitymas() const { return pazymiai; };
+
+        void vardo_nustatymas(string vardas) { this->vardas = vardas; };
+        void pavardes_nustatymas(string pavarde) { this->pavarde = pavarde; };
+        void vidurkio_nustatymas(double vidurkis) { this->vidurkis = vidurkis; };
+        void medianos_nustatymas(double mediana) { this->mediana = mediana; };
+        void egzamino_nustatymas(int egzaminas) { this->egzaminas = egzaminas; };
+        void pazymiu_nustatymas(vector<int> pazymiai) { this->pazymiai = pazymiai; };
+        void sortGrades() { sort(pazymiai.begin(), pazymiai.end()); };
+};
+
+void generuoti_faila_duomenis(int studentu_skaicius, int pazymiu_skaicius, const string& filename);
+void spausdinti_i_faila(const vector<studentas>& tmp, const string& filename);
+void vidurkio_radimas(studentas& tmp);
+vector<studentas> failo_nuskaitymas1(const string& filename);
+void medianos_radimas(studentas& tmp);
+void pazymiu_nuskaitymas(studentas& tmp);
+void studento_nuskaitymas(studentas& tmp);
+vector<studentas> failo_nuskaitymas2(const string& filename);
+void studento_spausdinimas(const vector<studentas>& tmp);
+vector<studentas> failo_nuskaitymas3(const string& filename);
